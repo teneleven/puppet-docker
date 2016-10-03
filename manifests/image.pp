3e971a2e8dd6c4dbc6665be26be2f16e9ecd2501
Michael Mackus <michaelmackus@gmail.com>
2016-10-03 16:49:33 -0700

    Move docker dir out & provision last

--- manifests/image.pp
+++ manifests/image.pp
@@ -1,7 +1,7 @@
 /* represents a base container that provisions itself using puppet */
 class dockerbridge::image (
   $image_name = 'base',
-  $docker_dir = 'puppet/modules/teneleven/'
+  $docker_dir = './'
 ) {
   docker::image { $image_name:
     docker_dir => $docker_dir,
--- manifests/init.pp
+++ manifests/init.pp
@@ -22,22 +22,6 @@ class dockerbridge (
     contain '::docker'
   }
 
-  if (!empty($provision)) {
-    $provision.each |$app_name, $app| {
-      $app_default_hosts = regsubst($default_hosts, '\$\{project_name\}', $app_name, 'GI')
-
-      create_resources('::dockerbridge::provision', {
-        $app_name => is_hash($app) ? {
-          true  => $app,
-          false => {
-            'type'  => $app,
-            'hosts' => any2array($app_default_hosts)
-          }
-        }
-      })
-    }
-  }
-
   if (!empty($images)) {
     $images.each |$name| {
       $options = $containers[$name] ? {
@@ -65,6 +49,22 @@ class dockerbridge (
       create_resources('dockerbridge::compose', { $app_name => {
         app_type => $app_type
       } })
+    }
+  }
+
+  if (!empty($provision)) {
+    $provision.each |$app_name, $app| {
+      $app_default_hosts = regsubst($default_hosts, '\$\{project_name\}', $app_name, 'GI')
+
+      create_resources('::dockerbridge::provision', {
+        $app_name => is_hash($app) ? {
+          true  => $app,
+          false => {
+            'app'   => $app,
+            'hosts' => any2array($app_default_hosts)
+          }
+        }
+      })
     }
   }
 }
