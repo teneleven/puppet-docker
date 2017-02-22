12e716ee83381da82754b66d7f4613e6b2b5d66b
Michael Mackus <michaelmackus@gmail.com>
2017-02-22 13:40:38 -0800

    Enable support for app_path fact

--- manifests/provision.pp
+++ manifests/provision.pp
@@ -1,5 +1,6 @@
 define dockerbridge::provision (
   $target = $title,
+  $path   = $title,
   $app    = undef,
   $hosts  = [],
 
@@ -27,19 +28,20 @@ define dockerbridge::provision (
         "FACTER_app_type=${app}",
         "FACTER_app_hosts=${host_str}",
         "FACTER_app_target=${target}",
+        "FACTER_app_path=${path}",
       ]}
     }
   } elsif ($dockerbridge::params::provision_with == 'docker_compose') {
     dockerbridge::compose { $title:
       app_name => $target,
       app_type => $app,
-      env      => ["COMPOSE_APP_TARGET=${target}", "COMPOSE_APP_TYPE=${app}", "COMPOSE_APP_HOSTS=${host_str}"]
+      env      => ["COMPOSE_APP_TARGET=${target}", "COMPOSE_APP_TYPE=${app}", "COMPOSE_APP_HOSTS=${host_str}", "COMPOSE_APP_PATH=${path}"]
     }
   }
 
   if ($dockerbridge::params::provision_with == 'shell') {
     $provision_args = merge({
-      env       => ["FACTER_project_name=${title}", "FACTER_app_type=${app}", "FACTER_app_hosts=${host_str}", "FACTER_app_target=${target}"],
+      env       => ["FACTER_project_name=${title}", "FACTER_app_type=${app}", "FACTER_app_hosts=${host_str}", "FACTER_app_target=${target}", "FACTER_app_path=${path}"],
     }, $extra_options)
   } else {
     $provision_args = $extra_options
